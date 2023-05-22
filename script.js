55de235689f2215e645b6792ed4e703d78f9bfb1
drivanreis <105787691+drivanreis@users.noreply.github.com>
2023-05-22 15:24:13 -0300

    Quase Fim do Front-end

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const arrNomes = ['Arroz', 'Feijão', 'Macarão', 'Agua', 'Farofa', 'Goma', 'Vinagre', 'Molho', 'Abel', 'Edu', 'Flavio', 'Gabriela', 'Helida', 'Itala', 'Jose', 'Lia', 'Marta', 'Neuma', 'Onofre', 'Pedro', 'Queni', 'Rita'];
+// Como agente não viu banco de dados o jeito foi improvisar.
+// São 3 arrays, que são trasformados em objetos e colocados 
+// dentro de outro array.
+const arrNomes = ['Arroz', 'Feijão', 'Macarão', 'Agua', 'Farofa', 'Goma', 'Vinagre', 'Molho', 'Mel', 'Cravo', 'Canela', 'Gengibre', 'Carne', 'Frango', 'Peixe', 'Camarão', 'Batata', 'Cenoura', 'Beterraba', 'Tomate', 'Cebola', 'Leite'];
 
 const arrPrecos = [1.3, 6.7, 3, 9.5, 1.4, 6, 1.4, 5.7, 4, 1.2, 0.9, 2.2, 1.3, 1.0, 2, 2.4, 3.3, 1, 4.4, 5.1, 0.25, 1.6];
 
@@ -19,18 +22,29 @@ const achaProduto = (nomeDoProduto) => arrTabelaDeProdutos.find((produto) =>  pr
 
 const criaListaDeProdutos = () => {
   arrTabelaDeProdutos.forEach((objElement, numLinha) => {
-    const divElem = document.createElement('div');
-    // trElem.setAttribut('id', `linha${linha}`);
-    // document.getElementById('lstProdutos').appendChild(trElem);
+    const trElem = document.createElement('tr');
+    trElem.setAttribute('id',`linha${numLinha}`);
+    document.getElementById('lstProdutos').appendChild(trElem);
     arrLinha = Object.values(objElement);
-    arrLinha.forEach((celula, coluna) => {
+    
+    arrLinha.forEach((celula, numColuna) => {
       console.log(celula);
-    //   const tdElem = document.createElement('td');
-    //   tdElem.id.add = `coluna${coluna}`
-    //   document.getElementById(`linha${linha}`).appendChild(tdElem);
-    //   tdElem.innerText = celula;
+      const tdElem = document.createElement('td');
+      tdElem.setAttribute('id', `coluna${numColuna}`);
+      document.getElementById(`linha${numLinha}`).appendChild(tdElem);
+      tdElem.innerText = celula;
     });
+
+    const iptNumero = document.createElement('input');
+    iptNumero.setAttribute('type', 'number');
+    document.getElementById(`linha${numLinha}`).appendChild(iptNumero);
+
+    const chkBox = document.createElement('input');
+    chkBox.setAttribute('type', 'checkbox');
+    document.getElementById(`linha${numLinha}`).appendChild(chkBox);
+
   });
+
 };
 
 criaListaDeProdutos();
